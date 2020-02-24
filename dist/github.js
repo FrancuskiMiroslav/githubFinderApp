583a6a8950602bc47cc7724aacba4f50f2a74a1c
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-24 12:20:18 +0100

    finished app

---
+++ dist/github.js
@@ -0,0 +1,26 @@
+class GitHub {
+  constructor() {
+    this.client_id = "bd44063a0316d9fe00f7";
+    this.client_secret = "4ed745ea89a2f63b334f0eb058e259b34f5c45af";
+    this.repos_count = 6;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const reposResponse = await fetch(
+      `http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile: profile,
+      repos: repos
+    };
+  }
+}
