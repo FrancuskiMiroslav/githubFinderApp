2d72e890901640cf1280070ea4ccae38e482623a
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-08-05 21:48:55 +0200

    fixed sticky footer

--- dist/github.js
+++ dist/github.js
@@ -8,11 +8,11 @@ class GitHub {
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
     const reposResponse = await fetch(
-      `http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
     const profile = await profileResponse.json();
